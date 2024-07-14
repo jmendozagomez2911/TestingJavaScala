85937a748e4046ba4527136a36429463f6b7979f
Jose Mendoza <jmendozagomez2911@outlook.com>
2024-07-14 20:40:38 +0200

    WIP on feature/linkedLists

--- Proyecto/src/main/java/javas/project/dataStructures/linkedList/SinglyLinkedList.java
+++ Proyecto/src/main/java/javas/project/dataStructures/linkedList/SinglyLinkedList.java
@@ -46,11 +46,11 @@ public class SinglyLinkedList {
 
         Node removeNode = tail;
 
-        if (head == tail) {
+        if (head == tail) {     //Check if the list has only one node
             head = tail = null;
         } else {
             Node currentNode = head;
-            while (currentNode.next != tail) {
+            while (currentNode.next != tail) {  //iterates through the list until it finds the node right before the tail.
                 currentNode = currentNode.next;
             }
             currentNode.next = null;
@@ -61,6 +61,37 @@ public class SinglyLinkedList {
         return removeNode;
     }
 
+
+    // Method to insert a node at a specified index
+    public boolean insert(int value, int index) {
+        if (index < 0 || index > size) {
+            return false;  // Invalid index
+        }
+
+        Node newNode = new Node();
+        newNode.value = value;
+
+        if (index == 0) {  // Insert at the head
+            newNode.next = head;
+            head = newNode;
+            if (size == 0) {
+                tail = newNode;
+            }
+        } else if (index == size) {  // Insert at the tail
+            tail.next = newNode;     // Correctly link the last node to the new node
+            tail = newNode;          // Update tail to the new node
+        } else {  // Insert in the middle
+            Node currentNode = head;
+            for (int i = 0; i < index - 1; i++) {
+                currentNode = currentNode.next;
+            }
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+        }
+
+        size++;
+        return true;
+    }
 
 
 }
